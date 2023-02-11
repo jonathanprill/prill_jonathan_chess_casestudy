c417d29b03b10ac64160967ef758af2dc6a4fcc1
jonathanprill <jonathantprill@gmail.com>
2023-02-11 09:51:24 -0500

    initial project setup

---
+++ src/main/java/com/prill/chess/PrillJonathanChessCasestudyApplication.java
@@ -0,0 +1,13 @@
+package com.prill.chess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrillJonathanChessCasestudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrillJonathanChessCasestudyApplication.class, args);
+	}
+
+}
